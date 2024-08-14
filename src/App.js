eb73b32d1f757274edca744e6e74ae66e31516bf
Pedro Henrique <pedrohenrique2937@gmail.com>
2024-08-14 17:19:37 -0300

    initialHub

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar.js';
+import { Banner } from './components/Banner.js';
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <div className="App">
-    
+    <NavBar />
+    <Banner />
     </div>
   );
 }
